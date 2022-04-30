9206333095ebc89dc33872c25c646ff9920ed5b6
NastasjaBakovic1999 <nastasjabakovic199@gmail.com>
2022-04-30 18:58:14 +0200

    implemented ServiceAdditionalServiceShipment

--- DeliveryServiceApp/Services/Implementation/ServiceAdditionalServiceShipment.cs
+++ DeliveryServiceApp/Services/Implementation/ServiceAdditionalServiceShipment.cs
@@ -3,6 +3,7 @@ using DeliveryServiceData.UnitOfWork;
 using DeliveryServiceDomain;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 
 namespace DeliveryServiceApp.Services.Implementation
@@ -18,37 +19,80 @@ namespace DeliveryServiceApp.Services.Implementation
 
         public void Add(AdditionalServiceShipment additionalServiceShipment)
         {
-            throw new NotImplementedException();
+            if (!IsValid(additionalServiceShipment)|| AlreadyExistInDB(additionalServiceShipment))
+            {
+                throw new ArgumentOutOfRangeException("Nevalidan unos!");
+            }
+
+            unitOfWork.AdditionalServiceShipment.Add(additionalServiceShipment);
+            unitOfWork.Commit();
+        }
+
+        private bool IsValid(AdditionalServiceShipment additionalServiceShipment)
+        {
+            bool valid = true;
+
+            if (additionalServiceShipment == null) return false;
+            if(additionalServiceShipment.Shipment == null || additionalServiceShipment.AdditionalService == null 
+               || additionalServiceShipment.AdditionalServiceId == 0 || additionalServiceShipment.ShipmentId == 0)
+            {
+                return false;
+            }
+
+            return valid;
+        }
+
+        private bool AlreadyExistInDB(AdditionalServiceShipment additionalServiceShipment)
+        {
+            bool exist = false;
+
+            if(unitOfWork.AdditionalServiceShipment.GetAll().Any(a => a.AdditionalServiceId == additionalServiceShipment.AdditionalServiceId && 
+                                                                    a.ShipmentId == additionalServiceShipment.ShipmentId))
+            {
+                return true;
+            }
+
+            return exist;
         }
 
         public void Delete(AdditionalServiceShipment additionalServiceShipment)
         {
-            throw new NotImplementedException();
+            if (additionalServiceShipment == null) throw new ArgumentException();
+            var adds = unitOfWork.AdditionalServiceShipment.FindByID(additionalServiceShipment.AdditionalServiceId, additionalServiceShipment.ShipmentId);
+            if(adds == null) throw new Exception();
+            unitOfWork.AdditionalServiceShipment.Delete(additionalServiceShipment);
+            unitOfWork.Commit();
         }
 
         public void Edit(AdditionalServiceShipment additionalServiceShipment)
         {
-            throw new NotImplementedException();
+            if (IsValid(additionalServiceShipment) == false) throw new ArgumentOutOfRangeException();
+            unitOfWork.AdditionalServiceShipment.Edit(additionalServiceShipment);
+            unitOfWork.Commit();
         }
 
         public AdditionalServiceShipment FindByID(int id, params int[] ids)
         {
-            throw new NotImplementedException();
+            var additionalServiceShipment = unitOfWork.AdditionalServiceShipment.FindByID(id, ids);
+            return additionalServiceShipment;
         }
 
         public List<AdditionalServiceShipment> GetAll()
         {
-            throw new NotImplementedException();
+            return unitOfWork.AdditionalServiceShipment.GetAll();
         }
 
         public List<AdditionalServiceShipment> GetByShipmentId(int shipmentId)
         {
-            throw new NotImplementedException();
+            var adds = unitOfWork.AdditionalServiceShipment.GetByShipmentId(shipmentId);
+            return adds;
         }
 
         public List<AdditionalServiceShipment> Search(Expression<Func<AdditionalServiceShipment, bool>> pred)
         {
-            throw new NotImplementedException();
+            if (pred == null) throw new Exception();
+            var adds = unitOfWork.AdditionalServiceShipment.Search(pred);
+            return adds;
         }
     }
 }
