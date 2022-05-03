9a74bbdd29215cd1c6a83a29ce5129dba1b1e074
NastasjaBakovic1999 <nastasjabakovic199@gmail.com>
2022-05-03 11:21:21 +0200

    implemented additionalServiceTest

---
+++ DeliveryServiceAppTests/AdditionalServiceTests.cs
@@ -0,0 +1,37 @@
+﻿using DeliveryServiceApp.Services.Implementation;
+using DeliveryServiceData.UnitOfWork;
+using DeliveryServiceDomain;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DeliveryServiceAppTests
+{
+    public class AdditionalServiceTests
+    {
+        Mock<IUnitOfWork> unitOfWork = Mocks.GetMockUnitOfWork();
+
+        [Fact]
+        public void TestServiceAdditionalServiceFindBy()
+        {
+            var service = new ServiceAdditionalService(unitOfWork.Object);
+            var result = service.FindByID(1);
+            var resultAdditionalService = Assert.IsType<AdditionalService>(result);
+            var expected = unitOfWork.Object.AdditionalService.FindByID(1);
+            Assert.Equal(expected.AdditionalServiceId, resultAdditionalService.AdditionalServiceId);
+        }
+
+        [Fact]
+        public void TestServiceAdditionalServiceFindByInvalid()
+        {
+            var service = new ServiceAdditionalService(unitOfWork.Object);
+            var result = service.FindByID(-4);
+
+            Assert.Null(result);
+        }
+    }
+}
