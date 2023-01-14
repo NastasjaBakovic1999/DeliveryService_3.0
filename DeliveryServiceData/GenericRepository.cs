f7e5fcba573c9f34257f09350ce24531f69df1b4
NastasjaBakovic199 <nastasjabakovic199@gmail.com>
2023-01-14 23:13:35 +0100

    added generic repository

---
+++ DeliveryServiceData/GenericRepository.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryServiceData
+{
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+        protected DbContext Context { get; }
+        public GenericRepository(DbContext context)
+        {
+            Context = context;
+        }
+
+        public virtual List<T> GetAll()
+        {
+            return Context.Set<T>().ToList();
+        }
+
+        public virtual T FindOneByExpression(Expression<Func<T, bool>> expression)
+        {
+            return Context.Set<T>().SingleOrDefault(expression);
+        }
+
+    }
+}
--- DeliveryServiceData/IRepository.cs
+++ DeliveryServiceData/IRepository.cs
@@ -1,11 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq.Expressions;
 
 namespace DeliveryServiceData
 {
     public interface IRepository<T> where T : class
     {
         List<T> GetAll();
-        T FindByID(int id, params int[] ids);
+        T FindOneByExpression(Expression<Func<T, bool>> expression);
     }
 }
