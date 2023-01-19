4eed789da7b127ad3c07339651d1c1119c959a77
NastasjaBakovic199 <nastasjabakovic199@gmail.com>
2023-01-19 22:50:58 +0100

    added configuration for view (shipment status statistic)

---
+++ DeliveryServiceDomain/Configurations/ShipmentStatusStatisticConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryServiceDomain.Configurations
+{
+    public class ShipmentStatusStatisticConfiguration :  IEntityTypeConfiguration<ShipmentStatusStatistic>
+    { 
+        public void Configure(EntityTypeBuilder<ShipmentStatusStatistic> entity)
+        {
+            entity.HasNoKey();
+            entity.ToView(nameof(ShipmentStatusStatistic));
+            entity.Property(e => e.StatusName).HasColumnName("StatusName");
+            entity.Property(e => e.NumberOfShipments).HasColumnName("NumberOfShipments");
+        }
+    }
+}
---
+++ DeliveryServiceDomain/ShipmentStatusStatistic.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryServiceDomain
+{
+    public class ShipmentStatusStatistic
+    {
+        public string StatusName { get; set; }
+        public int NumberOfShipments { get; set; }
+    }
+}
