61ccf08490028a48755d7dcdb5e580468d4eba64
NastasjaBakovic199 <nastasjabakovic199@gmail.com>
2023-01-19 22:52:46 +0100

    added inMemoryRepository for statistic also

---
+++ DeliveryServiceData/Implementation/InMemoryRepositoryShipmentStatusStatistic.cs
@@ -0,0 +1,26 @@
+﻿using DeliveryServiceDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryServiceData.Implementation
+{
+    public class InMemoryRepositoryShipmentStatusStatistic : IRepositoryShipmentStatusStatistic
+    {
+
+        private List<ShipmentStatusStatistic> shipmentStatusStatistics = new List<ShipmentStatusStatistic>();
+
+        public ShipmentStatusStatistic FindOneByExpression(Expression<Func<ShipmentStatusStatistic, bool>> expression)
+        {
+            return shipmentStatusStatistics.SingleOrDefault(expression.Compile());
+        }
+
+        public List<ShipmentStatusStatistic> GetAll()
+        {
+            return shipmentStatusStatistics;
+        }
+    }
+}
---
+++ DeliveryServiceData/Implementation/RepositoryShipmentStatusStatistic.cs
@@ -0,0 +1,17 @@
+﻿using DeliveryServiceDomain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryServiceData.Implementation
+{
+    public class RepositoryShipmentStatusStatistic : GenericRepository<ShipmentStatusStatistic>, IRepositoryShipmentStatusStatistic
+    {
+        public RepositoryShipmentStatusStatistic(DbContext context) : base(context)
+        {
+        }
+    }
+}
