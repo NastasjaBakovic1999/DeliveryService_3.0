403a2929b8b235df2686ad8a10b6de2fcc989be3
NastasjaBakovic199 <nastasjabakovic199@gmail.com>
2023-01-19 22:51:10 +0100

    services for statistic

---
+++ DeliveryServiceServices/Implementation/ServiceShipmentStatusStatistic.cs
@@ -0,0 +1,31 @@
+﻿using DeliveryServiceData.UnitOfWork;
+using DeliveryServiceDomain;
+using DeliveryServiceServices.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryServiceServices.Implementation
+{
+    public class ServiceShipmentStatusStatistic : IServiceShipmentStatusStatistic
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public ServiceShipmentStatusStatistic(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public ShipmentStatusStatistic FindByID(int id, params int[] ids)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ShipmentStatusStatistic> GetAll()
+        {
+            return unitOfWork.ShipmentStatusStatistic.GetAll();
+        }
+    }
+}
---
+++ DeliveryServiceServices/Interfaces/IServiceShipmentStatusStatistic.cs
@@ -0,0 +1,14 @@
+﻿using DeliveryServiceApp.Services;
+using DeliveryServiceDomain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeliveryServiceServices.Interfaces
+{
+    public interface IServiceShipmentStatusStatistic : IService<ShipmentStatusStatistic>
+    {
+    }
+}
